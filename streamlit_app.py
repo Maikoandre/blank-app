70fe969e1a194e631b9bbcbf385a859cf19a3637
Maiko André <155476255+Maikoandre@users.noreply.github.com>
2025-03-14 03:39:02 +0000

    Adicionado dataframes e slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("🎈 My streamlit new app")
 st.write(
@@ -11,4 +12,36 @@ df = pd.DataFrame({
     'second-column': [10,20,30,40]
 })
 
-df
+st.write(df)
+
+dataframe = np.random.randn(10,20)
+st.dataframe(dataframe)
+
+dataframe02 = pd.DataFrame(
+    np.random.randn(10,20),
+    columns=('col %d' % i for i in range(20))
+)
+
+st.dataframe(dataframe02.style.highlight_max(axis=0))
+
+st.table(dataframe02)
+
+char_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['a','b','c']
+)
+
+st.line_chart(char_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000,2)/[50,50]+[37.76, -122.4],
+    columns=['lat', 'lon']
+)
+
+st.map(map_data)
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+st.session_state.name
